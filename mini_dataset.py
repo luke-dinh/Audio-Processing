67275c45834d9f555ce7d393d255b4321b41149d
luke-dinh <dinhphuoclockt@gmail.com>
2021-12-08 22:40:10 +0700

    Update

--- mini_dataset.py
+++ mini_dataset.py
@@ -1,2 +1,12 @@
 import pandas as pd
+import argparse
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--num_frame", default = 20, type=int, help= "Number of data frames to slice")
+parser.add_argument("--save_path", default= "Audio-Processing", type=str, help="Save path")
+parser.parse_args()
+
+
+data_frame = pd.read_csv("train_meta.csv")
+mini_dataset = data_frame.head(20)
+mini_dataset.to_csv("mini_dataset.csv")
