224ec8a3842d19f708d9b185c1d1d7d7950ce3bc
luke-dinh <dinhphuoclockt@gmail.com>
2021-12-16 14:48:18 +0700

    Update

--- spectrogram.py
+++ spectrogram.py
@@ -1,7 +1,10 @@
 import librosa
 import os
 import IPython.display as ipd
+import sys
+
+sys.path.append(".")
 
 audio_hum = os.listdir("mini_dataset_hum")
 for file in audio_hum:
-    x, sr = librosa.load(file)
+    x, sr = librosa.core.load(file)
