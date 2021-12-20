76d380d48dc5c2093681bc49ae01cfc68d411b47
luke-dinh <dinhphuoclockt@gmail.com>
2021-12-20 16:20:06 +0700

    Update

--- spectrogram.py
+++ spectrogram.py
@@ -1,10 +1,33 @@
 import librosa
+import librosa.display as display
 import os
 import IPython.display as ipd
 import sys
+import matplotlib.pyplot as plt
 
 sys.path.append(".")
 
-audio_hum = os.listdir("mini_dataset_hum")
-for file in audio_hum:
-    x, sr = librosa.core.load(file)
+audio_path = "/home/lukedinh/Desktop/Audio-Processing/mini_dataset_hum/0000.mp3"
+# audio_hum = os.listdir("mini_dataset_hum")
+# audio_arr = []
+# ipd.Audio(audio_path)
+
+x, sr = librosa.core.load(audio_path)
+# plt.figure(figsize=(10,10))
+# display.waveplot(x, sr=sr)
+# plt.show()
+
+# for file in audio_hum:
+#     x, sr = librosa.core.load(file)
+#     audio_arr.append(x)
+
+# plt.figure(figsize=(10,10))
+# librosa.display.waveplot(audio_arr[0])
+
+# Spectrogram
+X = librosa.core.stft(x)
+Xdb = librosa.core.amplitude_to_db(abs(X))
+plt.figure(figsize=(10,10))
+display.specshow(Xdb, sr=sr, x_axis='time', y_axis='hz')
+plt.colorbar()
+plt.show()
